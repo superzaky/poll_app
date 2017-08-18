08204f10bb0344cf6c0b0f9929e6d8a62e3f4c14
superzaky <superkytoz@gmail.com>
2017-08-18 16:49:32 +0200

    Added configuration.

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'poll_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
