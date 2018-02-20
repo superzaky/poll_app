ea6c6b03b1f9301b3be39a14af35c5d049d63732
superzaky <superkytoz@gmail.com>
2018-02-20 21:51:52 +0100

    Add form.

--- polls/urls.py
+++ polls/urls.py
@@ -7,6 +7,22 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    #When somebody requests a page from your website say, '/polls/34/', Django will load the
+    # mysite.urls Python module because it's pointed to by the 'ROOT_URLCONF' setting. It finds the 
+    #variable named urlpatterns and traverses the regular expressions in order. After finding the
+    # match at '^polls/', it strips off the matching text ("polls/") and sends the remaining 
+    #text - "34/" - to the 'polls.urls' URLconf for further processing. There it matches
+    # r'^(?P<question_id>[0-9]+)/$', resulting in a call to the detail() view like so:
+    
+    #    detail(request=<HttpRequest object>, question_id='34')
+    
+    #The question_id='34' part comes from (?P<question_id>[0-9]+). Using parentheses around a pattern
+    # 'captures' the text matched by that pattern and sends it as an argument to the view function;
+    # ?P<question_id> defines the name that will be used to identify the matched pattern; and [0-9]+ is
+    # a regular expression to match a sequence of digits (i.e., a number). Because the URL
+    # patterns are regular expressions, there really is no limit on what you can do with them. 
+ 
+    
     # the 'name' value as called by the {% url %} template tag
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
--- polls/views.py
+++ polls/views.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -29,8 +30,33 @@ def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
+#The code for our vote() view does have a small problem. It first gets the selected_choice object from the 
+#database, then computes the new value of votes, and then saves it back to the database. If two users of 
+#your website try to vote at exactly the same time, this might go wrong: The same value, let’s say 42, will
+# be retrieved for votes. Then, for both users the new value of 43 is computed and saved, but 44 would be
+# the expected value.
+
+#This is called a race condition. If you are interested, you can read 'Avoiding race conditions using F()' to
+# learn how you can solve this issue.
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
