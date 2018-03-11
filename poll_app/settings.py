93f79fde2eab3985f4189e73de6876adb2beb943
superzaky <superkytoz@gmail.com>
2018-03-11 22:58:05 +0100

    Customizing project template.

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'poll_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #DIRS is a list of filesystem directories to check when loading Django templates; it's a search path.
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
